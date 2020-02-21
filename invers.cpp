b06d80ecc77e6fee8f965b61be546fe45489a911
als698 <39515296+als698@users.noreply.github.com>
2020-02-21 17:08:09 +0200

    new

---
+++ invers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+        int n,x=0;
+        cout<<"n=";
+        cin>>n;
+        while (n!=0){
+          x=x*10+n%10;
+          n=n/10;
+        }
+        cout<<x<<endl;
+}
